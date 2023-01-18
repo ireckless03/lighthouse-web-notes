9f7cabf7c83f8f30ee8d298b90f0bfd2144056f0
Denny Doan <denny-333@hotmail.com>
2023-01-18 18:49:35 -0500

    Added keybindings to input

--- snake-client/input.js
+++ snake-client/input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.on("data", handleUserInput);
   stdin.setRawMode(true);
@@ -10,6 +13,24 @@ const setupInput = function () {
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
+  }
+
+  if (key === 'w') {
+    connection.write("Move: up")
+  }
+
+  if (key === 'a') {
+    connection.write("Move: left")
+  }
+
+  if (key === 's') {
+    connection.write("Move: down"
+
+    )
+  }
+
+  if (key === 'd') {
+    connection.write("Move: right")
   }
 };
 
